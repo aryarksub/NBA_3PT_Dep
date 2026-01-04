36ae5f0a69a0a0416ee19702d72dc2f0756dfdf2
Arya Kondur <aryarksub@gmail.com>
2026-01-04 16:27:29 +0530

    Add plots for paper and make folder for paper submissions private

--- src/plots.py
+++ src/plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+import matplotlib.image as mpimg
 
 PLOTS_DIR = 'plots'
 
@@ -46,3 +47,26 @@ def plot_cis(dfs, x_col, mean_col='mean', low_col='mean_ci_lower', high_col='mea
         plt.savefig(save_file, dpi=300)
     else:
         plt.show()
+
+def merge_three_png_plots(file_paths, fname):
+    """
+    Merge three PNG plots side-by-side.
+
+    Args:
+        file_paths (list): List of paths to PNG files
+        fname (str): File name to save merged figure
+    """
+    assert len(file_paths) == 3
+
+    # Create a figure with 1 row and 3 columns
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+    # Loop through each image file and add it to the plot
+    for i, file_path in enumerate(file_paths):
+        img = mpimg.imread(file_path)
+        axes[i].imshow(img)
+        axes[i].axis('off')
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+    plt.savefig(fname, dpi=300)
